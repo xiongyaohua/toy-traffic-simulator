e96a7911c041ae78ea4aa2695cae6007b0d0d5c9
Yaohua Xiong <xiongyaohua@gmail.com>
2023-03-04 14:19:39 +0800

    viewport transform

--- simulator/engine.py
+++ simulator/engine.py
@@ -1,17 +1,69 @@
 from typing import Sequence
+import numpy as np
+from PySide6.QtGui import QVector2D, QTransform
+
 
 class Car:
-    pass
+    def get_position(self) -> QVector2D:
+        raise NotImplementedError
+
+    def get_heading(self) -> QVector2D:
+        raise NotImplementedError
+
+    def get_transform(self) -> QTransform:
+        position = self.get_position()
+        heading = self.get_heading()
+        t = QTransform()
+        t.translate(*position.toTuple())
+        r = np.arctan2(*heading.toTuple())
+        t.rotateRadians(r)
+
+        return t
 
 class Road:
     pass
+
 
 class World:
     def __init__(self):
         pass
 
     def get_cars(self) -> Sequence[Car]:
-        pass
+        raise NotImplementedError
 
     def get_roads(self) -> Sequence[Road]:
-        pass
+        raise NotADirectoryError
+
+
+class DummyCar(Car):
+    def __init__(self, x, y, dx, dy):
+        self.position = QVector2D(x, y)
+        self.heading = QVector2D(dx, dy).normalized()
+
+    def get_position(self) -> QVector2D:
+        return self.position
+
+    def get_heading(self) -> QVector2D:
+        return self.heading
+
+
+class DummyWorld(World):
+    def __init__(self, n: int = 100, w: float = 1000, h: float = 800):
+        self.n = n
+        self.w = w
+        self.h = h
+        self._generate_cars()
+
+    def get_cars(self) -> Sequence[Car]:
+        return self.cars
+
+    def _generate_cars(self):
+        xs = np.random.rand(self.n) * self.w
+        ys = np.random.rand(self.n) * self.h
+        rs = np.random.rand(self.n) * np.pi * 2.0
+
+        self.cars = []
+
+        for x, y, r in zip(xs, ys, rs):
+            car = DummyCar(x, y, np.cos(r), np.sin(r))
+            self.cars.append(car)
--- simulator/gui.py
+++ simulator/gui.py
@@ -1,15 +1,46 @@
 from PySide6.QtWidgets import QMainWindow
-from PySide6.QtGui import QPainter
-from PySide6.QtCore import QPointF
+from PySide6.QtGui import QPainter, QTransform, QVector2D
+#from PySide6.QtCore import QPointF
+import numpy as np
+from .engine import DummyWorld, World
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.resize(1200, 800)
         print("Hello MainWindow")
+        self.world = DummyWorld()
+        self.camera = Camera()
 
     def paintEvent(self, event) -> None:
         with QPainter(self) as painter:
-            painter.drawLine(QPointF(10, 10), QPointF(500, 300))
+            painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+            self.camera.paint(painter, self.width(), self.height(), self.world)
 
         return super().paintEvent(event)
+    
+class Camera:
+    def __init__(self) -> None:
+        self.center = QVector2D(300, 300)
+        self.scale = QVector2D(0.5, 0.5)
+
+    def paint(self, painter: QPainter, width: int, height: int, world: World):
+        vt = self.get_transform(width, height)
+        painter.setTransform(vt)
+        painter.drawPoint(300,300)
+        for car in world.get_cars():
+            t = car.get_transform()
+            painter.setTransform(t * vt)
+            
+            self.draw_car(car, painter)
+
+    def draw_car(self, car, painter: QPainter):
+        painter.drawRect(-20, -10, 40, 20)
+        painter.fillRect(10, -6, 5, 12, "Blue")
+
+    def get_transform(self, width, height) -> QTransform:
+        t = QTransform()
+        t.translate(-self.center.x()+width/2, self.center.y()+height/2)
+        t.scale(self.scale.x(), -self.scale.y())
+        return t
