dffbd152b72da9f0f27e3b95264036bfaed773d6
Yaohua Xiong <xiongyaohua@gmail.com>
2023-03-10 10:23:13 +0800

    refactor

--- simulator/lane.py
+++ simulator/lane.py
@@ -1,32 +1,7 @@
 from typing import Sequence, Optional
-import numpy as np
-from numpy.linalg import norm
 from .car import Car
-from .util import Vec2, vec2_to_array
 from .range import Range
-
-class CenterLine:
-    def __init__(self, p1: Vec2, p2: Vec2):
-        self.p1 = vec2_to_array(p1)
-        self.p2 = vec2_to_array(p2)
-        self.length = norm(self.p1 - self.p2)
-        self.tangent = (self.p2 - self.p1) / self.length
-        self.normal = np.array([self.tangent[1], -self.tangent[0]])
-
-    def get_length(self):
-        return self.length
-
-    def tesselate(self) -> list:
-        return [self.p1, self.p2]
-    
-    def sample_at(self, pos: float) -> tuple[Vec2, Vec2]:
-        p = self.p1 + pos * self.tangent
-        return (p, self.tangent)
-    
-    def get_offseted(self, offset):
-        p1 = self.p1 + self.normal * offset
-        p2 = self.p2 + self.normal * offset
-        return CenterLine(p1, p2)
+from .center_line import CenterLine
 
 class Lane:
     def __init__(self, center_line: CenterLine):
